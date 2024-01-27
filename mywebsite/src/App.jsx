7fdfe0cb3e7ed4f6efed45f95bdb4df6fddf2cca
prathamesh195 <157907062+prathamesh195@users.noreply.github.com>
2024-01-27 17:42:52 +0000

    new-done

--- mywebsite/src/App.jsx
+++ mywebsite/src/App.jsx
@@ -7,13 +7,22 @@ function App() {
   return (
     <> 
    <div className='w-full h-screen bg-blue-200'>
-        <div className='bg-red-400 w-full h-10 text-center  '>
+        <div className='bg-red-400 w-full text-center p-10 '>
             Welcome to my website
       </div>
-    <div className='bg-blue-100 flex items-center'>
+    <div className='bg-blue-100 flex items-center flex-col'>
       Prathamesh Sawant From Bicholim
+      <img 
+    className=' rounded-full'
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIbm4m6qwdQruO7mmsKaWth9J9-hnGx3YOD1tdqEGgyw&s" alt="" />
     </div>
-    <div></div>
+  
+    <div>
+      <div className='bg-red-200 w-full h-20'>
+aas
+      </div>
+
+    </div>
 
 
    </div>
